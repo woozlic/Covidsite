faa40f9b1b6fad96a4a6b9e77c5d4cfad94447d2
woozlic <sampaccs@mail.ru>
2020-10-22 11:04:13 +0500

    added login page

--- views.py
+++ views.py
@@ -3,4 +3,9 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', sidebar=True)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    return render_template('login.html', sidebar=False)
